07ebed71565d89f699648a2c1f936afce306b95d
w01f <arsenimarchenko@bk.ru>
2024-10-02 21:27:43 +0300

    fix: Remove message from ErrorMessage component and some UI changes

--- src/renderer/src/components/shared/UI/Button.tsx
+++ src/renderer/src/components/shared/UI/Button.tsx
@@ -25,9 +25,9 @@ const Button: FC<ButtonProps> = ({
     <button
       {...props}
       className={clsx(
-        'flex items-center justify-center rounded-2xl font-medium disabled:opacity-50 text-xl px-12 py-2.5 relative before:absolute before:rounded-2xl before:inset-0 before:transition before:opacity-20 transition duration-100 enabled:active:scale-95 select-none',
+        'flex items-center shadow-md justify-center rounded-2xl font-medium disabled:opacity-50 text-xl px-12 py-2.5 relative before:absolute before:rounded-2xl before:inset-0 before:transition before:opacity-20 transition duration-100 enabled:active:scale-95 select-none',
         {
-          'bg-white shadow-md text-blue enabled:hover:before:bg-blue_dark enabled:active:before:bg-blue_dark before:opacity-10':
+          'bg-white text-blue enabled:hover:before:bg-blue_dark enabled:active:before:bg-blue_dark before:opacity-10':
             role === 'secondary',
           'bg-blue text-white enabled:hover:before:bg-blue_light enabled:active:before:bg-blue_light':
             role === 'primary',
